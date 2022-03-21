099dc924186200450691c454a9f810213cf34e3d
SameerInstacash <sameer.khan@getinstacash.in>
2022-03-21 11:10:07 +0530

    Initial Commit

---
+++ InstaCashSDK/InstaCashSDK.h
@@ -0,0 +1,18 @@
+//
+//  InstaCashSDK.h
+//  InstaCashSDK
+//
+//  Created by Sameer Khan on 21/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InstaCashSDK.
+FOUNDATION_EXPORT double InstaCashSDKVersionNumber;
+
+//! Project version string for InstaCashSDK.
+FOUNDATION_EXPORT const unsigned char InstaCashSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstaCashSDK/PublicHeader.h>
+
+
